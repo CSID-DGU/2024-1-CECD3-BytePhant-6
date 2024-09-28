2f06ad159f5a5b78c3e9349d453db10bbf69433d
shortboy7 <thdwldnd7@naver.com>
2024-09-29 01:39:44 +0900

    feat: chat screen에 텍스트 입력 기능 추가 및 focus 컨트롤 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/chat/ChatScreen.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/chat/ChatScreen.kt
@@ -1,9 +1,118 @@
 package com.bytephant.senior_care.ui.screen.chat
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.bytephant.senior_care.R
+import com.bytephant.senior_care.data.BaseMessage
+import com.bytephant.senior_care.ui.screen.chat.bubble.AIChatBubbleBody
+import com.bytephant.senior_care.ui.screen.chat.bubble.ChatBubbleList
+import com.bytephant.senior_care.ui.screen.chat.bubble.UserChatBubbleBody
 
 @Composable
 fun ChatScreen(
-
+    chatViewModel: ChatViewModel = viewModel(),
+    modifier: Modifier = Modifier
 ) {
+    val uiState by chatViewModel.uiState.collectAsState()
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(uiState.messages.size, listState.isScrollInProgress) {
+        if (uiState.messages.isNotEmpty()) {
+            listState.animateScrollToItem(uiState.messages.size - 1)
+        }
+    }
+    LaunchedEffect(uiState.isSending) {
+        if (!uiState.isSending) {
+            focusRequester.requestFocus()
+        }
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .imePadding()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = { focusManager.clearFocus() })
+                }
+        ) {
+            ChatBubbleList(
+                listState = listState,
+                messages = uiState.messages
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                value = uiState.inputText,
+                onValueChange = { chatViewModel.updateInputText(it) },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+                    .align(Alignment.Bottom)
+                    .focusRequester(focusRequester),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                ),
+                enabled = !uiState.isSending,
+            )
+            Button(
+                onClick = {
+                    if (uiState.inputText.isNotBlank()) {
+                        chatViewModel.sendQuestion(uiState.inputText)
+                    }
+                },
+                enabled = !uiState.isSending,
+                modifier = Modifier
+                    .padding(end = 16.dp)
+            ) {
+                Text(
+                    text="보내기",
+                )
+            }
+        }
+    }
 }
