4bb4fb6172dfe75109b703450ee65923e2bcc875
shortboy7 <thdwldnd7@naver.com>
2024-09-29 04:45:38 +0900

    feat: viewModel에 replier 의존성 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/chat/ChatViewModel.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/chat/ChatViewModel.kt
@@ -3,6 +3,7 @@ package com.bytephant.senior_care.ui.screen.chat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bytephant.senior_care.data.BaseMessage
+import com.bytephant.senior_care.domain.replier.Replier
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,13 +11,16 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
-class ChatViewModel : ViewModel() {
+class ChatViewModel(
+    private val replier : Replier
+) : ViewModel() {
     private val _uiState = MutableStateFlow(ChatUiState())
     val uiState = _uiState.asStateFlow()
 
     fun sendQuestion(sentence: String) {
+        val userMessage = BaseMessage(sentence)
+
         _uiState.update { currentState ->
-            val userMessage = BaseMessage(sentence)
 
             currentState.copy(
                 messages = currentState.messages + userMessage,
@@ -24,8 +28,7 @@ class ChatViewModel : ViewModel() {
             )
         }
         viewModelScope.launch {
-            val replyMessage : BaseMessage = BaseMessage("reply", false)
-
+            val replyMessage : BaseMessage = replier.reply(sentence)
             withContext(Dispatchers.Main) {
                 _uiState.update { currentState ->
                     currentState.copy(
