0837bd0d30e066e88e4b71ad90b4b86d30957f94
shortboy7 <thdwldnd7@naver.com>
2024-09-29 12:51:08 +0900

    fix: 누락된 네트워크 설정 추가 - body 어노테이션 - network 권한 사용 허용

--- frontend/app/src/main/java/com/bytephant/senior_care/service/network/api/MessageAPI.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/service/network/api/MessageAPI.kt
@@ -1,6 +1,7 @@
 package com.bytephant.senior_care.service.network.api
 
 import kotlinx.serialization.Serializable
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import java.time.LocalDateTime
@@ -26,5 +27,5 @@ interface MessageAPI {
     @GET("init-message")
     suspend fun getInitMessage() : InitMessageRes
     @POST("reply")
-    suspend fun getReply(req: ReplyReq) : ReplyRes
+    suspend fun getReply(@Body req: ReplyReq) : ReplyRes
 }
