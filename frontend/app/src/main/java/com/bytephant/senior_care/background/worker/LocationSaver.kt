a667ec51563efe2c5aa7484135ff7c083e738cc1
shortboy7 <thdwldnd7@naver.com>
2024-10-07 23:11:01 +0900

    feat: workManager에 디버그 정보 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/background/worker/LocationSaver.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/background/worker/LocationSaver.kt
@@ -2,10 +2,12 @@ package com.bytephant.senior_care.background.worker
 
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.Data
 import androidx.work.WorkerParameters
 import com.bytephant.senior_care.application.SeniorCareApplication
 import com.bytephant.senior_care.domain.data.UserLocationStatus
 import com.bytephant.senior_care.service.database.entity.UserLocation
+import com.bytephant.senior_care.service.location.LocationClient
 import java.time.LocalDateTime
 
 class LocationSaver(
@@ -17,7 +19,13 @@ class LocationSaver(
         val container = (context.applicationContext as SeniorCareApplication).container
         val locationClient = container.locationClient
         val locationRepository = container.locationRepository
+        val dataBuilder = Data.Builder()
+            .putString("init", "init")
         try {
+            if (!LocationClient.checkPermission(context)) {
+                throw LocationClient.LocationException("no permissions")
+            }
+            dataBuilder.putString("permissions", "permissions: pass")
             locationClient.getCurrentLocation().collect { location ->
                 locationRepository.saveLocation(
                     location.longitude,
@@ -25,9 +33,14 @@ class LocationSaver(
                     UserLocationStatus.HOME
                 )
             }
+            dataBuilder.putString("location", "location: pass")
         } catch (e: Exception) {
-            return Result.failure()
+            return Result.failure(
+                dataBuilder
+                    .putString("ERROR_MESSAGE", e.message)
+                    .build()
+            )
         }
-        return Result.success()
+        return Result.success(dataBuilder.build())
     }
 }
