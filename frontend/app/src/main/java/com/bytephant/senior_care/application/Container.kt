30c194d165beaa7f97ef63131273b8e57dce44dc
송지웅 <thdwldnd7@dgu.ac.kr>
2024-10-10 03:05:33 +0900

    feat: tts 모듈 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/application/Container.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/application/Container.kt
@@ -1,6 +1,8 @@
 package com.bytephant.senior_care.application
 
 import android.content.Context
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import androidx.room.Room
 import com.bytephant.senior_care.domain.replier.MockReplier
 import com.bytephant.senior_care.domain.replier.NetworkReplier
@@ -13,10 +15,11 @@ import com.bytephant.senior_care.service.location.LocationClient
 import com.bytephant.senior_care.service.network.RetrofitConfig
 import com.bytephant.senior_care.service.network.api.MessageAPI
 import com.google.android.gms.location.LocationServices
+import java.util.Locale
 
-class Container(
+class Container (
     private val context : Context
-) {
+) : TextToSpeech.OnInitListener {
     private val messageAPI : MessageAPI by lazy {
         RetrofitConfig.retrofit.create(MessageAPI::class.java)
     }
@@ -39,6 +42,19 @@ class Container(
             context = context,
             LocationServices.getFusedLocationProviderClient(context)
         )
+    }
+
+    val ttsService : TextToSpeech = TextToSpeech(context, this);
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = ttsService.setLanguage(Locale.KOREAN)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "한국어 미지원")
+            }
+        } else {
+            Log.e("TTS", "TTS 초기화 실패");
+        }
     }
 
 }
