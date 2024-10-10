aa61960e74bc5fb0304d1821152b6491e97a67fe
shortboy7 <thdwldnd7@naver.com>
2024-10-10 14:35:25 +0900

    feat: HomeViewModel factory 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/home/HomeViewModel.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/home/HomeViewModel.kt
@@ -1,6 +1,11 @@
 package com.bytephant.senior_care.ui.screen.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bytephant.senior_care.application.SeniorCareApplication
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -8,4 +13,14 @@ class HomeViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(HomeState())
     val uiState =_uiState.asStateFlow()
 
+    companion object {
+        val Factory : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as SeniorCareApplication)
+                val replier = application.container.replier
+                val tts = application.container.ttsService
+                HomeViewModel()
+            }
+        }
+    }
 }
