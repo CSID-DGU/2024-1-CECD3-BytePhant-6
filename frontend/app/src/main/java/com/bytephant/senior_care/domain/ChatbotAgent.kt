6ae77e44c6272e77ef861083b495897358c9f67d
shortboy7 <thdwldnd7@naver.com>
2024-10-14 02:05:12 +0900

    feat: ChatbotAgent 메서드의 반환값에 BaseMessage 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/domain/ChatbotAgent.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/domain/ChatbotAgent.kt
@@ -10,15 +10,17 @@ class ChatbotAgent(
     private val replier: Replier,
     private val speaker: Speaker,
 ) {
-    suspend fun initTalking() {
+    suspend fun initTalking() : BaseMessage{
         val message = replier.initDialogue()
         dialogueHolder.appendMessage(message)
         speaker.speak(message.message)
+        return message
     }
 
-    suspend fun reply(userMessage: BaseMessage) {
+    suspend fun reply(userMessage: BaseMessage) : BaseMessage {
         val message = replier.reply(userMessage.message)
         speaker.speak(message.message)
         dialogueHolder.appendMessage(message)
+        return message
     }
 }
