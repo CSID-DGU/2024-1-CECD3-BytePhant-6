45dbd9c32b1653acf1364aea090e22ad22637875
shortboy7 <thdwldnd7@naver.com>
2024-11-27 02:49:58 +0900

    feat: 기억 정보를 얻어 오는 기능 추가

--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/HistoryViewModel.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/HistoryViewModel.kt
@@ -1,15 +1,49 @@
 package com.bytephant.senior_care.ui.screen.history
 
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.bytephant.senior_care.application.SeniorCareApplication
+import com.bytephant.senior_care.domain.user.MockUserMetaLoader
+import com.bytephant.senior_care.domain.user.UserMetaLoader
+import com.bytephant.senior_care.ui.screen.home.HomeViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class HistoryViewModel(
+    private val userMetaLoader: UserMetaLoader
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(HistoryUiState())
-    val uiState = _uiState.asStateFlow()
+    val uiState: StateFlow<HistoryUiState> = _uiState.asStateFlow()
 
-    fun updateHistory() {
-
+    fun loadDailyHistories() {
+        viewModelScope.launch {
+            try {
+                val histories = userMetaLoader.loadHistory()
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        history = histories,
+                    )
+                }
+            } catch (ignored: Exception) {
+            }
+        }
     }
+    companion object {
+        val Factory : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val container = (this[APPLICATION_KEY] as SeniorCareApplication).container
+
+                HistoryViewModel(MockUserMetaLoader())
+            }
+        }
+    }
+
 }
