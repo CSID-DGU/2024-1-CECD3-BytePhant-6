f542845a26aae017f56d4646b2e2189768ed520a
shortboy7 <thdwldnd7@naver.com>
2024-11-27 21:09:31 +0900

    feat: FavoriteScreen 상태 추가

---
+++ frontend/app/src/main/java/com/bytephant/senior_care/domain/data/FavoriteKeyword.kt
@@ -0,0 +1,6 @@
+package com.bytephant.senior_care.domain.data
+
+data class FavoriteKeyword(
+    val name: String,
+    val score: Int
+)
---
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/favorite/FavoriteUiState.kt
@@ -0,0 +1,7 @@
+package com.bytephant.senior_care.ui.screen.favorite
+
+import com.bytephant.senior_care.domain.data.FavoriteKeyword
+
+data class FavoriteUiState(
+    val keywordList: List<FavoriteKeyword> = listOf()
+)
