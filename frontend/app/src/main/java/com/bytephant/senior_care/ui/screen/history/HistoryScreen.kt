94ea5c3a32bb559c172c6f180445f7ee10496c7a
shortboy7 <thdwldnd7@naver.com>
2024-11-28 00:41:32 +0900

    fix: history 화면 formatter 최적화

--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/HistoryScreen.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/HistoryScreen.kt
@@ -3,16 +3,10 @@ package com.bytephant.senior_care.ui.screen.history
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -20,12 +14,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.bytephant.senior_care.ui.screen.history.tab.HistoryTab
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
 fun HistoryScreen(
     viewModel: HistoryViewModel,
     modifier: Modifier = Modifier
 ) {
+    val formatter = DateTimeFormatter.ofPattern("M월 d일 EEEE", Locale.KOREAN)
     val uiState by viewModel.uiState.collectAsState()
     val listState = rememberLazyListState()
 
@@ -41,13 +38,13 @@ fun HistoryScreen(
         LazyColumn(
             state = listState,
             modifier = Modifier
-                .fillMaxSize()
                 .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(uiState.history) { dailyHistory ->
                 HistoryTab(
                     dailyHistory = dailyHistory,
+                    formatter,
                     modifier = Modifier.padding(8.dp)
                 )
             }
--- frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/tab/HistoryTab.kt
+++ frontend/app/src/main/java/com/bytephant/senior_care/ui/screen/history/tab/HistoryTab.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DateRange
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.sharp.KeyboardArrowDown
 import androidx.compose.material.icons.sharp.KeyboardArrowUp
@@ -31,7 +29,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,9 +42,9 @@ import java.util.Locale
 @Composable
 fun HistoryTab(
     dailyHistory: DailyHistory,
+    formatter: DateTimeFormatter,
     modifier: Modifier = Modifier
 ) {
-    val formatter = DateTimeFormatter.ofPattern("M월 d일 EEEE", Locale.KOREAN)
     var isExpanded by remember { mutableStateOf(true) }
 
 
@@ -149,5 +146,7 @@ fun HistoryTabPreview() {
     HistoryTab(dailyHistory = DailyHistory(
         date = LocalDate.now(),
         logs = listOf("abc:def", "abc:grt")
-    ))
+    ),
+        formatter = DateTimeFormatter.ofPattern("M월 d일 EEEE", Locale.KOREAN),
+    )
 }
